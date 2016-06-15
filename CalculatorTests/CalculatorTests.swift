1ee32b53840a8cd4cd5fc51a35eeb2a14f675821
Denys Meloshyn <ned1988@gmail.com>
2016-06-15 20:17:22 +0300

    Add more tests and Readme

--- CalculatorTests/CalculatorTests.swift
+++ CalculatorTests/CalculatorTests.swift
@@ -33,6 +33,28 @@ class CalculatorTests: XCTestCase {
         XCTAssertEqual(c, result)
     }
     
+    func testAddNegative() {
+        CalculatorManager.shareInstance.reset()
+        
+        let a = NSDecimalNumber(integer: -2)
+        let b = NSDecimalNumber(integer: -3)
+        let c = NSDecimalNumber(integer: -5)
+        
+        let result = CalculatorManager.shareInstance.add(a, secondValue: b)
+        
+        XCTAssertEqual(c, result)
+    }
+    
+    func testAddToMaxNumber() {
+        CalculatorManager.shareInstance.reset()
+        
+        let a = NSDecimalNumber.maximumDecimalNumber()
+        let b = NSDecimalNumber(integer: 1)
+        
+        CalculatorManager.shareInstance.add(a, secondValue: b)
+        XCTAssertNotEqual(CalculatorManager.shareInstance.calculationError, NSCalculationError.NoError)
+    }
+    
     func testSubtracting() {
         CalculatorManager.shareInstance.reset()
         
@@ -43,6 +65,28 @@ class CalculatorTests: XCTestCase {
         let result = CalculatorManager.shareInstance.subtracting(a, secondValue: b)
         
         XCTAssertEqual(c, result)
+    }
+    
+    func testSubtractingNegative() {
+        CalculatorManager.shareInstance.reset()
+        
+        let a = NSDecimalNumber(integer: -2)
+        let b = NSDecimalNumber(integer: -3)
+        let c = NSDecimalNumber(integer: 1)
+        
+        let result = CalculatorManager.shareInstance.subtracting(a, secondValue: b)
+        
+        XCTAssertEqual(c, result)
+    }
+    
+    func testSubtractingMinimum() {
+        CalculatorManager.shareInstance.reset()
+        
+        let a = NSDecimalNumber.minimumDecimalNumber()
+        let b = NSDecimalNumber(integer: 1)
+        
+        CalculatorManager.shareInstance.subtracting(a, secondValue: b)
+        XCTAssertNotEqual(CalculatorManager.shareInstance.calculationError, NSCalculationError.NoError)
     }
     
     func testMultiplying() {
@@ -57,12 +101,48 @@ class CalculatorTests: XCTestCase {
         XCTAssertEqual(c, result)
     }
     
+    func testMultiplyingNegative() {
+        CalculatorManager.shareInstance.reset()
+        
+        let a = NSDecimalNumber(integer: -2)
+        let b = NSDecimalNumber(integer: -3)
+        let c = NSDecimalNumber(integer: 6)
+        
+        let result = CalculatorManager.shareInstance.multiplying(a, secondValue: b)
+        
+        XCTAssertEqual(c, result)
+    }
+    
     func testDividing() {
         CalculatorManager.shareInstance.reset()
         
         let a = NSDecimalNumber(integer: 6)
         let b = NSDecimalNumber(integer: 3)
         let c = NSDecimalNumber(integer: 2)
+        
+        let result = CalculatorManager.shareInstance.dividing(a, secondValue: b)
+        
+        XCTAssertEqual(c, result)
+    }
+    
+    func testDividingNegative() {
+        CalculatorManager.shareInstance.reset()
+        
+        let a = NSDecimalNumber(integer: -6)
+        let b = NSDecimalNumber(integer: -3)
+        let c = NSDecimalNumber(integer: 2)
+        
+        let result = CalculatorManager.shareInstance.dividing(a, secondValue: b)
+        
+        XCTAssertEqual(c, result)
+    }
+    
+    func testDividingZeroByNumber() {
+        CalculatorManager.shareInstance.reset()
+        
+        let a = NSDecimalNumber(integer: 0)
+        let b = NSDecimalNumber(integer: 6)
+        let c = NSDecimalNumber(integer: 0)
         
         let result = CalculatorManager.shareInstance.dividing(a, secondValue: b)
         
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Calculator
+
+## Task:
+- Create a simple calculator, that can "+", "-", "*", "/", "clear", and "=" 
+- Model, logic and UI must be separated
+- Unit tests must be implemnted
+
+## Possible improvements:
+- add possibility to enter float values
+- add possibility to reverse sign of the value
+- add iPad support
+- add possibility to execute multiple arithmetic command in one line
+- add UI test
+- add more mathematical operators
